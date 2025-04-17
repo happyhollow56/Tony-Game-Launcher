a47ecaa966d99a622c306a1ff6fa850b899f515c
happyhollow56 <oaklystokes@gmail.com>
2025-04-17 16:40:04 -0400

    Update tinker.py

--- tinker.py
+++ tinker.py
@@ -23,18 +23,10 @@ frame1.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
 frame1.columnconfigure(0, weight=1)
 frame1.rowconfigure(1, weight=1)
 
-entry1=tk.Entry(frame1)
-entry1.grid(row=0, column=0, sticky="ew")
-
-entry1.bind("<Return>", lambda event: add_to_list(entry1, text_list1))
-
-entry1_btn=tk.Button(frame1, text="add", command=lambda: add_to_list(entry1, text_list1))
+entry1_btn=tk.Button(root, text="Open Guild Wars 2", command=ope_Gw2)
 entry1_btn.grid(row=0, column=1)
 
-text_list1 = tk.Listbox(frame1)
-text_list1.grid(row=1, column=0, columnspan=2, sticky="nsew")
-
-# frame 2
+#frame 2
 
 frame2 = tk.Frame(root)
 frame2.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
